46eb1404df7315bb82114f8755051680dd558534
Gert-Jan Compagner <gert.jan.compagner@ing.nl>
2019-07-22 21:27:56 +0200

    Add notifier.js

---
+++ docker-production/notifier.js
@@ -0,0 +1,46 @@
+const ghnotifier = require('google-home-notifier')
+const Setting = require('../api/models/Setting')
+const Promise = require('bluebird')
+const axios = require('axios')
+
+function speak (device, text, options) {
+  let promises = []
+
+  if (!options) {
+    options = {}
+  }
+
+  if (!options.lang) {
+    promises.push(Setting.findOne({identifier: 'default-language'}).then((defaultLanguageSetting) => {
+      options.lang = defaultLanguageSetting.value
+    }))
+  }
+
+  return Promise.all(promises).then(() => {
+    ghnotifier.device(device.name, options.lang)
+    ghnotifier.ip(device.ip_address)
+    return new Promise((resolve, reject) => {
+      if (text.indexOf('.mp3') >= 0) {
+        return ghnotifier.play(text, (result) => {
+          if (result === 'error') {
+            reject(new Error(`Notification failed on device ${device.name}`))
+          } else {
+            resolve()
+          }
+        })
+      } else {
+        return ghnotifier.notify(text, (result) => {
+          if (result === 'error') {
+            reject(new Error(`Notification failed on device ${device.name}`))
+          } else {
+            resolve()
+          }
+        })
+      }
+    })
+  })
+}
+
+module.exports = {
+  speak
+}
